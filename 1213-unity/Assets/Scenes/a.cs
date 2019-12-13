9ff9a09d51adcbd836632efdfe4c2f5e8ffd72a2
BI1061109 <58834318+BI1061109@users.noreply.github.com>
2019-12-13 15:13:05 +0800

    更新

--- 1213-unity/Assets/Scenes/a.cs
+++ 1213-unity/Assets/Scenes/a.cs
@@ -8,9 +8,43 @@ public class a : MonoBehaviour {
     public float speed = 1.5f;
     [Header("血量"), Range(100, 1000)]
     public float hp = 100;
-    public Animator ani;
-    public Rigidbody2D r2D;
+    
+    private Animator ani;
+    private Rigidbody2D r2d;
+    private AudioSource aud;
+
+    [Header("子彈物件")]
+    public GameObject bullte;
+    [Header("發射音效")]
+    public AudioClip soundFire ;
+
+    public Transform point;
+    public float speedBullet=1000;
 
 #endregion
 
+private void Start(){
+    ani = GetComponent<Animator>();
+    r2d = GetComponent<Rigidbody2D>();
+    aud = GetComponent<AudioSource>();
+}
+private void FixedUpdate() {
+    Move();
+}
+private void Update() {
+    Fire();
+}
+private void Move(){
+    float h = Input.GetAxisRaw("Horizontal");
+    r2d.AddForce(new Vector2(speed*h,0));
+    if (h!=0) transform.localScale= new Vector3(h,1,1);
+}
+private void Fire(){
+    if(Input.GetKeyDown(KeyCode.Mouse0))
+    {
+        aud.PlayOneShot(soundFire);
+        GameObject temp = Instantiate(bullte,point.position,Quaternion.identity);
+        temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(speedBullet*transform.localScale.x,0));
+    }
+}
 }
