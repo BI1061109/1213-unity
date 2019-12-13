b498be77291bbd3b46a0d90cac8e7717e614b2c5
BI1061109 <58834318+BI1061109@users.noreply.github.com>
2019-12-13 12:14:11 +0800

    1213-Unity2D

---
+++ 1213-unity/Assets/Scenes/a.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour {
+#region
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 1.5f;
+    [Header("血量"), Range(100, 1000)]
+    public float hp = 100;
+    public Animator ani;
+    public Rigidbody2D r2D;
+
+#endregion
+
+}
